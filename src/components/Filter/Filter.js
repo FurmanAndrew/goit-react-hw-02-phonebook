be3c6105dca46f308de1cee6872be5a756c28d6c
FurmanAndrew <103194679+FurmanAndrew@users.noreply.github.com>
2023-05-15 17:31:06 +0000

    Deploying to gh-pages from @ FurmanAndrew/goit-react-hw-02-phonebook@2ce65ff050750335bf21d783d50d13de819c8978 🚀

--- src/components/Filter/Filter.js
+++
@@ -1,9 +0,0 @@
-import css from './Filter.module.css'
-
-const Filter = ({search, onChange}) => {
-    return(<>
-    <label htmlFor='filterContacts' className={css.filerContacts}>Find contacts by name<input type="text" name="filter" value={search} onChange={onChange}/></label>
-    </>)
-}
-
-export default Filter
